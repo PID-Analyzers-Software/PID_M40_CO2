f6212f14c51e823212eb0ccc6f8990682900801a
Zhijie Wang <zjwa127@gmail.com>
2024-05-31 12:27:32 +0800

    Update ConfigurationManager.h

--- a_PID_M40_CO2/inc/ConfigurationManager.h
+++ a_PID_M40_CO2/inc/ConfigurationManager.h
@@ -18,25 +18,25 @@ public:
 class ConfigurationManager : public ParamChangeListener
 {
 
-    const uint16_t EEPROM_SIZE = sizeof(int) + 				// TIMER
-							sizeof(double) * (2 + 8)	// GASES
-							+ 208;
+    const uint16_t EEPROM_SIZE = sizeof(int) * (1 + 1 + 3) + // Timer, Selected Gas Index, Range, Alarm, Calibration
+                                 sizeof(double) * 8 + // Gas calibration parameters (intercept + slope for 4 gases)
+                                 64 + // Device ID
+                                 32 * 2; // Wi-Fi SSID and Password
 	
-    const uint8_t EEPROM_TIMER_OFFSET = 999;
-    const uint8_t EEPROM_GAS_SLOPE_OFFSET = 999;
-    const uint8_t EEPROM_GAS_SELECT_OFFSET = 80;
+    const uint8_t EEPROM_TIMER_OFFSET = 0;
+    const uint8_t EEPROM_GAS_SELECT_OFFSET = 4;
 
-    const uint8_t EEPROM_GAS_INTERCEPT_OFFSET = 12;
-    const uint8_t EEPROM_GAS_SECONDP_OFFSET = 20;
+    const uint8_t EEPROM_GAS_INTERCEPT_OFFSET = 8;
+    const uint8_t EEPROM_GAS_SLOPE_OFFSET = 16;
 
-    const uint8_t EEPROM_GAS_INTERCEPT2_OFFSET = 28;
-    const uint8_t EEPROM_GAS_SECONDP2_OFFSET = 36;
+    const uint8_t EEPROM_GAS_INTERCEPT2_OFFSET = 24;
+    const uint8_t EEPROM_GAS_SLOPE2_OFFSET = 32;
 
-    const uint8_t EEPROM_GAS_INTERCEPT3_OFFSET = 44;
-    const uint8_t EEPROM_GAS_SECONDP3_OFFSET = 52;
+    const uint8_t EEPROM_GAS_INTERCEPT3_OFFSET = 40;
+    const uint8_t EEPROM_GAS_SLOPE3_OFFSET = 48;
 
-    const uint8_t EEPROM_GAS_INTERCEPT4_OFFSET = 60;
-    const uint8_t EEPROM_GAS_SECONDP4_OFFSET = 68;
+    const uint8_t EEPROM_GAS_INTERCEPT4_OFFSET = 56;
+    const uint8_t EEPROM_GAS_SLOPE4_OFFSET = 64;
 
 
     //const uint8_t EEPROM_GAS_AIR_TC_OFFSET = 28;
@@ -45,12 +45,12 @@ class ConfigurationManager : public ParamChangeListener
     //const uint8_t EEPROM_GAS_He_TC_OFFSET = 52;
     //const uint8_t EEPROM_GAS_H2_TC_OFFSET = 60;
     //const uint8_t EEPROM_GAS_ArCH4_TC_OFFSET = 68;
-    const uint8_t EEPROM_DEVICE_ID_OFFSET = 132;			//64 chars
-    const uint8_t EEPROM_WIFI_SSID_OFFSET = 164;		//32 chars
-    const uint8_t EEPROM_WIFI_PASSWORD_OFFSET = 196;	//32 chars
-    const uint8_t EEPROM_RANGE_OFFSET = 132;	//uint16_t
-    const uint8_t EEPROM_Calvalue_OFFSET = 150;	//uint16_t
-    const uint8_t EEPROM_ALARM_OFFSET = 162;	//uint16_t
+    const uint8_t EEPROM_DEVICE_ID_OFFSET = 84;			//64 chars
+    const uint8_t EEPROM_WIFI_SSID_OFFSET = 148;		//32 chars
+    const uint8_t EEPROM_WIFI_PASSWORD_OFFSET = 180;	//32 chars
+    const uint8_t EEPROM_RANGE_OFFSET = 72;	//uint16_t
+    const uint8_t EEPROM_Calvalue_OFFSET = 80;	//uint16_t
+    const uint8_t EEPROM_ALARM_OFFSET = 76;	//uint16_t
 
 
     const uint8_t EEPROM_GAS_AIR_TC_OFFSET = 999;
@@ -90,9 +90,8 @@ public:
 
     void saveGasSelectedToEEPROM(int gasIndex, bool doCommit = true);
 
-    void saveSlopeToEEPROM(double slope, bool doCommit = true);
 	void saveInterceptToEEPROM(double intercept, bool doCommit = true);
-    void saveSecondpToEEPROM(double secondp, bool doCommit = true);
+    void saveSlopeToEEPROM(double slope, bool doCommit = true);
     void saveGasThermalConductivity(String gasName, double tc, bool doCommit = true);
 	void saveWifiSSID(String ssid, bool doCommit = true);
 	void saveWifiPassword(String password, bool doCommit = true);
